4025bd53009f59b5bb9336012c3754edb4ecd011
Medorna <mgedorna@hotmail.com>
2022-12-23 13:59:04 -0300

    va nuevo

--- wify.py
+++ wify.py
@@ -1,16 +1,7 @@
-# Importamos la librería Scapy y algunas otras librerías necesarias
+# Importamos las librerías necesarias
 import scapy.all as scapy
 import os
-
-# Creamos una función para escanear las redes WiFi
-
-
-def scan_wifi():
-    # Enviamos un paquete de broadcast para solicitar a los puntos de acceso que se identifiquen
-    broadcast = scapy.Ether(dst="00000000000000000")
-    wifi_probe_request = scapy.Dot11ProbeReq()
-    packet = broadcast / wifi_probe_request
-    iface = "Wi-Fi"
+from time import sleep
 
 # Creamos una función de callback para procesar las respuestas de los puntos de acceso
 
@@ -18,15 +9,26 @@ def scan_wifi():
 def wifi_callback(packet):
     # Si el paquete es una respuesta de punto de acceso (subtype=5), procesamos la información
     if packet.haslayer(scapy.Dot11ProbeResp):
-        # Extraemos la dirección MAC del punto de acceso y el nombre de la red (SSID)
-        ap_mac = packet.addr2
-        ap_name = packet.info
+        # Extraemos la direccion MAC del punto de acceso y el nombre de la red (SSID)
+        mac = packet.addr2
+        ssid = packet.info.decode()
 
         # Mostramos la información en pantalla
-        print(f"Dirección MAC: {ap_mac} | SSID: {ap_name}")
+        print(f"Dirección MAC: {mac} | SSID: {ssid}")
 
 
+        # Creamos un bucle para escanear las redes WiFi cada 5 segundos
 # Creamos un bucle para escanear las redes WiFi cada 5 segundos
 while True:
-    scan_wifi()
-    scapy.sniff(iface="Wi-Fi", prn=wifi_callback, store=0)
+    # Enviamos un paquete de broadcast para solicitar a los puntos de acceso que se identifiquen
+    broadcast = scapy.Ether(dst="00000000000000000")
+    wifi_probe_request = scapy.Dot11ProbeReq()
+    packet = broadcast / wifi_probe_request
+
+    # Enviamos el paquete de broadcast y procesamos las respuestas de los puntos de acceso
+    ans, _ = scapy.sr(packet, iface="Wi-Fi", timeout=1)
+    for packet in ans:
+        wifi_callback(packet)
+
+    # Detenemos la ejecución durante 5 segundos
+    sleep(5)
